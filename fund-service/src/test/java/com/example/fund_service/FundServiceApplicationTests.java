270126ae710b5500b7cc38a051e49fc4d4e7832e
buibup <mongkol@emetworks.com>
2025-06-28 15:22:51 +0700

    init

---
+++ fund-service/src/test/java/com/example/fund_service/FundServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.fund_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FundServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
